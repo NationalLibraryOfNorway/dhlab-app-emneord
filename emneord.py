c6654fbaf21f472a065c9073d31ae77f83b421cd
iciclejj <55782191+iciclejj@users.noreply.github.com>
2025-02-10 11:39:02 +0100

    Refactor corpus_defined verification

--- emneord.py
+++ emneord.py
@@ -17,40 +17,7 @@ def get_topic_counts(corpus, column='subjects'):
     emner = pd.DataFrame.from_dict(emneord, orient='index', columns=["frekvens"]).sort_values(by = 'frekvens', ascending=False)
     return emner
 
-st.set_page_config(
-    page_title="Metadata", 
-    page_icon=None, 
-    layout="wide",
-    initial_sidebar_state="auto", 
-    menu_items=None
-)
-
-st.sidebar.markdown("Velg et korpus fra [corpus-appen](https://beta.nb.no/dhlab/corpus/)" 
-                    " eller hent en eller flere URNer fra nb.no eller andre steder")
-
-
-corpus_defined = False
-urner = st.sidebar.text_area("Lim inn URNer:","", help="Lim en tekst som har URNer i seg. Teksten trenger ikke å være formatert")
-if urner != "":
-    urns = re.findall(r"URN:NBN[^\s.,]+", urner)
-    if urns != []:
-        corpus_defined = True
-        corpus = dh.Corpus(doctype='digibok',limit=0)
-        corpus.extend_from_identifiers(urns)
-    else:
-        st.write('Fant ingen URNer')
-
-uploaded_file = st.sidebar.file_uploader("Last opp et korpus", help="Dra en fil over hit, fra et nedlastningsikon, eller velg fra en mappe")
-if uploaded_file is not None:
-    corpus_defined = True
-    dataframe = pd.read_excel(uploaded_file)
-
-    corpus = dh.Corpus(doctype='digibok',limit=0)
-    corpus.extend_from_identifiers(list(dataframe.urn))
-
-st.header('Inspiser metadata')
-
-if corpus_defined:
+def process_corpus(corpus: dh.Corpus):
     corpusdf = corpus.corpus.fillna("")
     corpusdf.year = pd.to_datetime(corpusdf.year.map(lambda x:str(int(x))), infer_datetime_format=True)
     corpusdf.timestamp = pd.to_datetime(corpusdf.timestamp.map(lambda x:str(int(x))), infer_datetime_format=True)
@@ -71,16 +38,16 @@ if corpus_defined:
     df = get_topic_counts(corpusdf, gruppering)
     if percent == True:
         df = (df*100/df.sum())
-    
+
     colA, colB = st.columns(2)
-    
+
     with colA:
         st.write(f"### Opptelling av _{gruppering}_")
         if percent == True:
             st.write(df.style.format(precision=2))
         else:
             st.write(df)
-    
+
     with colB:
         st.write(f"### Totaler for korpuset")
         if percent == True:
@@ -89,7 +56,40 @@ if corpus_defined:
             st.write(f"Sum over alle elementer i _{gruppering}_ blir {int(df[df.index != ''].sum())}, av totalt  {int(df.sum())} inkludert blanke _{gruppering}_.")
         st.write(f"Antall _{gruppering}_ er {len(df)}.")
         st.write(f"Korpusstørrelsen er {len(corpusdf)}.")
-         
-else:
-    st.write(' -- venter på korpus --')
 
+
+st.set_page_config(
+    page_title="Metadata",
+    page_icon=None,
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items=None
+)
+
+st.sidebar.markdown("Velg et korpus fra [corpus-appen](https://beta.nb.no/dhlab/corpus/)" 
+                    " eller hent en eller flere URNer fra nb.no eller andre steder")
+
+
+corpus = None
+
+urner = st.sidebar.text_area("Lim inn URNer:","", help="Lim en tekst som har URNer i seg. Teksten trenger ikke å være formatert")
+if urner != "":
+    urns = re.findall(r"URN:NBN[^\s.,]+", urner)
+    if urns != []:
+        corpus = dh.Corpus(doctype='digibok',limit=0)
+        corpus.extend_from_identifiers(urns)
+    else:
+        st.write('Fant ingen URNer')
+
+uploaded_file = st.sidebar.file_uploader("Last opp et korpus", help="Dra en fil over hit, fra et nedlastningsikon, eller velg fra en mappe")
+if uploaded_file is not None:
+    dataframe = pd.read_excel(uploaded_file)
+    corpus = dh.Corpus(doctype='digibok',limit=0)
+    corpus.extend_from_identifiers(list(dataframe.urn))
+
+st.header('Inspiser metadata')
+
+if corpus is None:
+    st.write(' -- venter på korpus --')
+else:
+    process_corpus(corpus)
